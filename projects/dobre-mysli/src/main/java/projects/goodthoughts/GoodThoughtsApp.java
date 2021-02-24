efb91489d416312164f27c5e1dec9a3ede3d5055
honestit <michal.kupisinski@gmail.com>
2021-02-24 19:24:30 +0100

    2021-02-24 :: Dobre myśli :: 1. Utworzenie i konfiguracja projektu

---
+++ projects/dobre-mysli/src/main/java/projects/goodthoughts/GoodThoughtsApp.java
@@ -0,0 +1,14 @@
+package projects.goodthoughts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GoodThoughtsApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(GoodThoughtsApp.class);
+
+    public static void main(String[] args) {
+        System.out.println("Witaj");
+        logger.info("Uruchamianie aplikacji ...");
+    }
+}
