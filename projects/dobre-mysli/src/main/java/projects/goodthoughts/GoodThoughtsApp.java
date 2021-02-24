833b3290a4e8a4670f1d62435dd1ac8a4e369f28
honestit <michal.kupisinski@gmail.com>
2021-02-24 21:12:38 +0100

    2021-02-24 :: Dobre myśli :: 3. Utworzenie bazowego interfejsu użytkownika

--- projects/dobre-mysli/src/main/java/projects/goodthoughts/GoodThoughtsApp.java
+++ projects/dobre-mysli/src/main/java/projects/goodthoughts/GoodThoughtsApp.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import projects.goodthoughts.model.Quote;
 
 import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.Set;
 
 public class GoodThoughtsApp {
 
@@ -24,5 +26,83 @@ public class GoodThoughtsApp {
         logger.debug("Test cytatu 1: " + quote1 + " - koniec cytatu");
         logger.debug("Test cytatu 2: {} - koniec cytatu", quote2);
         logger.debug(String.format("Oba cytaty, 1: %s, 2: %s", quote1, quote2));
+
+        GoodThoughtsApp app = new GoodThoughtsApp();
+        app.run();
+    }
+
+    public void run(Object... args) {
+        logger.info("Rozpoczęcie pracy z aplikacją");
+        showWelcomeMessage();
+        while (true) {
+            showMenu();
+            String userInput = getUserInput();
+            boolean isValidOption = validateOption(userInput);
+            if (isValidOption) {
+                executeOption(userInput);
+                boolean isExitOption = checkIsExitOption(userInput);
+                if (isExitOption) {
+                    break;
+                }
+            } else {
+                showInvalidOptionMessage(userInput);
+            }
+        }
+        showGoodbyeMessage();
+        logger.info("Zakończono pracę z aplikacją");
+    }
+
+    private void showGoodbyeMessage() {
+        System.out.println("Dziękujemy za skorzystanie z naszej aplikacji. Wróć tu jeszcze!");
+    }
+
+    private void showInvalidOptionMessage(String userInput) {
+        System.out.println("Wybrałeś nieprawidłową opcję ('" + userInput + "')");
+    }
+
+    private boolean checkIsExitOption(String userInput) {
+//        return userInput.equals("0");
+        return "0".equals(userInput);
+    }
+
+    private void executeOption(String userInput) {
+        //TODO Agnieszka
+    }
+
+    private boolean validateOption(String userInput) {
+        Set<String> validOptions = Set.of("0", "1", "2", "3");
+        return validOptions.contains(userInput);
+//        switch (userInput) {
+//            case "0":
+//            case "1":
+//            case "2":
+//            case "3":
+//                return true;
+//            default:
+//                return false;
+//        }
+//        if (userInput.equals("1")) {
+//            return true;
+//        }
+//        return false;
+    }
+
+    private String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nTwój wybór: ");
+        String userInput = scanner.nextLine();
+        return userInput.trim();
+    }
+
+    private void showMenu() {
+        System.out.println("Wybierz opcje z listy poniżej:");
+        System.out.println("\t1. Pobierz domyślny cytat");
+        System.out.println("\t2. Dodaj własny cytat");
+        System.out.println("\t3. Wyświetl zapisane cytaty");
+        System.out.println("\t0. Zakończ");
+    }
+
+    private void showWelcomeMessage() {
+        System.out.println("Witaj w naszej aplikacji (" + LocalDateTime.now() + ")");
     }
 }
